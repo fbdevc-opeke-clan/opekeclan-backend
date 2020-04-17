455157c983cbfac63a7d985714e083d4a56215ec
marusoft <alimi.kmaruf@gmail.com>
2020-04-17 11:28:09 +0100

    (install eslint): setup eslint

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import logger from 'morgan';
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+
+app.use('*', (req, res) => res.status(404).send({
+  message: 'App not Found',
+}));
+
+app.use('/', (req, res) => res.status(200).send({
+  message: 'Welcome Opeke Backend App',
+}));
+
+
+export default app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
